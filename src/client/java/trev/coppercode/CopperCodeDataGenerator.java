00b3ae79a16e896543ba38224f34cda37df48d70
toki <clevertrever31@gmail.com>
2026-03-13 15:29:00 -0500

    Added rest of fabric template

---
+++ src/client/java/trev/coppercode/CopperCodeDataGenerator.java
@@ -0,0 +1,11 @@
+package trev.coppercode;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperCodeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
